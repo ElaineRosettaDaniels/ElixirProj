c682820e2b85884e202308e10d3813033ddd777a
ArtificerEllie <elainedaniels457@gmail.com>
2020-12-13 19:14:30 -0500

    adding card shuffle test file

---
+++ cards/lib/cards.ex
@@ -0,0 +1,44 @@
+defmodule Cards do
+    @moduledoc """
+    Documentation for `Cards`.
+    """
+
+    def create_deck() do
+        suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+        values = ["Ace", "Two", "Three", "Four"]
+
+        cards = for suit <- suits, value <- values do
+                    "#{value} of #{suit}"
+                end
+    end
+
+
+    def shuffle(deck) do
+        Enum.shuffle(deck)
+    end
+
+    def deal(deck, hand_size) do
+        Enum.split(deck, hand_size)
+    end
+
+    def create_hand(hand_size) do
+        Cards.create_deck
+        |> Cards.shuffle
+        |> Cards.deal(hand_size)
+    end
+
+    def save(deck, filename) do
+        binary = :erlang.term_to_binary(deck)
+        File.write(filename, binary)
+    end
+
+    def load(filename) do
+        case File.read(filename) do
+            {:ok, binary} -> :erlang.binary_to_term binary
+            {:error, _reason} -> "That file does not exist."
+        end
+    end
+
+
+
+end
