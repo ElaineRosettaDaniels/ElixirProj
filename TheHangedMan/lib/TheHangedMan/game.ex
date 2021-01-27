9ac322af200aee6c882f1eef3fef1846be81e691
ElaineRosettaDaniels <elainedaniels457@gmail.com>
2021-01-27 17:11:01 -0500

    wip

--- TheHangedMan/lib/TheHangedMan/game.ex
+++ TheHangedMan/lib/TheHangedMan/game.ex
@@ -7,10 +7,14 @@ defmodule TheHangedMan.Game do
     used: MapSet.new()
   )
 
-  def new_game() do
+  def new_game(word) do
     %TheHangedMan.Game{
-      letters: Dictionary.random_word |> String.codepoints
+      letters: word |> String.codepoints
     }
+  end
+
+  def new_game() do
+    new_game(Dictionary.random_word())
   end
 
   def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
@@ -38,8 +42,15 @@ defmodule TheHangedMan.Game do
     Map.put(game, :game_state, new_state)
   end
 
-  def score_guess(game, _not_good_guess) do
-    game
+  def score_guess(game = %{turns_left: 1}, _not_good_guess) do
+    Map.put(game, :game_state, :lost)
+  end
+
+  def score_guess(game = %{turns_left: turns_left}, _not_good_guess) do
+    %{ game | 
+      game_state: :bad_guess,
+      turns_left: turns_left - 1
+    }
   end
 
   def tally(game) do
--- TheHangedMan/test/game_test.exs
+++ TheHangedMan/test/game_test.exs
@@ -31,5 +31,35 @@ defmodule GameTest do
     assert game.game_state == :already_used
   end
 
+  test "a good guess is recognized" do
+    game = Game.new_game("cats")
+    {game, _tally} = Game.make_move(game, "c")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    game = Game.new_game("cats")
+    {game, _tally} = Game.make_move(game, "c")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _tally} = Game.make_move(game, "a")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _tally} = Game.make_move(game, "t")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _tally} = Game.make_move(game, "s")
+    assert game.game_state == :won
+    assert game.turns_left == 7
+  end
+
+  test "bad guess is recognized" do
+    game = Game.new_game("cats")
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+
 
 end
