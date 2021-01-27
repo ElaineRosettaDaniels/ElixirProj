6b8072b2ce7fb1fa0767014e8dc27337bfbd5e0b
ElaineRosettaDaniels <elainedaniels457@gmail.com>
2021-01-27 13:52:16 -0500

    fixed name

--- the_hanged_man/lib/thehangedman/game.ex
+++ the_hanged_man/lib/thehangedman/game.ex
@@ -1,12 +1,15 @@
 defmodule TheHangedMan.Game do
 
-defstruct(
-  turns_left: 7,
-  game_state: :initializing,
-)
+  defstruct(
+    turns_left: 7,
+    game_state: :initializing,
+    letters: [],
+  )
 
   def new_game() do
-    %Hangman.Game{}
+    %TheHangedMan.Game{
+      letters: Dictionary.random_word |> String.codepoints
+    }
   end
 
 end
