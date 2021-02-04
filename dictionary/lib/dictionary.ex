37cd826fb4b542018409de27cb0f55a3d1d4593c
ArtificerEllie <elainedaniels457@gmail.com>
2021-02-04 11:15:23 -0500

    wip

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,26 +1,8 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for `Dictionary`.
-  """
 
-  @doc """
-  Hello world.
+    alias Dictionary.WordList
 
-  ## Examples
-
-      iex> Dictionary.hello()
-
-
-  """
-  def random_word() do
-    Enum.random(word_list())
-  end
-
-  def word_list do
-    "../assets/words.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> String.split(~r/\n/)
-  end
+    defdelegate start(), to: WordList, as: :word_list
+    defdelegate random_word(word_list), to: WordList
 
 end
